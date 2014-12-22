bddbb1a43b1774ab2e1a1d94f8cf34b61ba589ce
Philip Rieck <me@philiprieck.com>
2014-12-22 14:49:59 -0600

    update nuget package number

--- WpfAppBar/Properties/AssemblyInfo.cs
+++ WpfAppBar/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1-alpha")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
