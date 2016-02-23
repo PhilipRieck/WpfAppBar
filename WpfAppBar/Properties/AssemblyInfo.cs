c6455207e958d1e1e90dd3030726d0bd6235adb0
Philip Rieck <me@philiprieck.com>
2016-02-23 09:55:31 -0600

    Fix resize loop on windows 10

--- WpfAppBar/Properties/AssemblyInfo.cs
+++ WpfAppBar/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
-[assembly: AssemblyInformationalVersion("1.4.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyInformationalVersion("1.5.0")]
