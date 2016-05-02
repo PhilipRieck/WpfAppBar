93c961389a5e837cfbf1535935678e518aa174a6
Philip Rieck <me@philiprieck.com>
2016-05-02 14:19:27 -0500

    update package to 1.6

--- WpfAppBar/Properties/AssemblyInfo.cs
+++ WpfAppBar/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
-[assembly: AssemblyInformationalVersion("1.5.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyInformationalVersion("1.6.0")]
