8487722c9465760738280058e0e989aa67cf7928
Philip Rieck <me@philiprieck.com>
2018-03-23 13:22:13 -0500

    push package

--- WpfAppBar/Properties/AssemblyInfo.cs
+++ WpfAppBar/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
-[assembly: AssemblyInformationalVersion("1.6.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyInformationalVersion("1.7.0")]
